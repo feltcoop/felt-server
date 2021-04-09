d8ac4d1eabe5eb631e5040c4087fd0f09abede42
greatbacon <hamiltonjreed@gmail.com>
2021-04-09 16:54:21 -0600

    Update deploy command with stubs & some Linode init steps

--- src/deploy.task.ts
+++ src/deploy.task.ts
@@ -7,16 +7,24 @@ export const task: Task = {
     description: 'deploy felt server to prod',
     run: async ({invokeTask}) => {        
         //TODO - add more dynamic naming
-        await spawnProcess('tar',['-cvf','deploy.tar','dist']);        
+        await spawnProcess('tar',['-cvf','deploy.tar','dist', 'package.json', 'package-lock.json']);        
         //scp to server
-        //your ssh key will need to be added account
-        await spawnProcess(
-            `ssh root@96.126.116.174 && ` +
-            `ls`,
-            [],
-            {shell: true},
-        );
+        //your ssh key will need to be added to linode account
+        await spawnProcess('scp', ['deploy.tar', 'root@96.126.116.174:deploy.tar']);                                            
         //unpack        
+        //// tar -xvf deploy.tar        
         //start/restart server
+        //// npm i
+        //// npm start
 	},    
 }
+
+
+//Linode Ubuntu Initialization Steps
+// sudo apt-get unzip
+// curl -fsSL https://fnm.vercel.app/install | bash
+// source /root/.bashrc
+// fnm install 14.16.0
+// sudo apt-get npm
+
+
