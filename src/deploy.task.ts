a3ba730799902201850ed458b00b1595687ec2ac
greatbacon <hamiltonjreed@gmail.com>
2021-04-12 08:33:03 -0600

    gro format

--- src/deploy.task.ts
+++ src/deploy.task.ts
@@ -2,20 +2,24 @@ import type {Task} from '@feltcoop/gro/dist/task/task.js';
 import {spawnProcess} from '@feltcoop/gro/dist/utils/process.js';
 import {DIST_DIR_NAME, paths} from '@feltcoop/gro/dist/paths.js';
 
-
 export const task: Task = {
-    description: 'deploy felt server to prod',
-    run: async ({invokeTask}) => {        
-        //TODO - add more dynamic naming
-        await spawnProcess('tar',['-cvf','deploy.tar', DIST_DIR_NAME, 'package.json', 'package-lock.json']);        
-        //scp to server
-        //your ssh key will need to be added to linode account
-        await spawnProcess('scp', ['deploy.tar', 'root@96.126.116.174:deploy.tar']);                                            
-        //unpack & start server
-        await spawnProcess('ssh', ['root@96.126.116.174', 'tar -xvf deploy.tar;', 'npm i;'])            
-	},    
-}
-
+	description: 'deploy felt server to prod',
+	run: async ({invokeTask}) => {
+		//TODO - add more dynamic naming
+		await spawnProcess('tar', [
+			'-cvf',
+			'deploy.tar',
+			DIST_DIR_NAME,
+			'package.json',
+			'package-lock.json',
+		]);
+		//scp to server
+		//your ssh key will need to be added to linode account
+		await spawnProcess('scp', ['deploy.tar', 'root@96.126.116.174:deploy.tar']);
+		//unpack & start server
+		await spawnProcess('ssh', ['root@96.126.116.174', 'tar -xvf deploy.tar;', 'npm i;']);
+	},
+};
 
 //Linode Ubuntu Initialization Steps
 // sudo apt-get unzip
@@ -23,5 +27,3 @@ export const task: Task = {
 // source /root/.bashrc
 // fnm install 14.16.0
 // sudo apt-get npm
-
-
