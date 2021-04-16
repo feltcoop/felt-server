0151bdb4d6981af67535b841e30f927647da5867
greatbacon <hamiltonjreed@gmail.com>
2021-04-16 14:19:30 -0600

    revert destroy to close (why did it change in the first place?)

--- src/server/ApiServer.ts
+++ src/server/ApiServer.ts
@@ -163,7 +163,7 @@ export class ApiServer {
 
 	async destroy(): Promise<void> {
 		await Promise.all([
-			this.db.destroy(),
+			this.db.close(),
 			new Promise((resolve, reject) =>
 				// TODO remove type casting when polka types are fixed
 				((this.app.server as any) as Server).close((err) => (err ? resolve : reject(err))),
