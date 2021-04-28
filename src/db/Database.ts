e861f7345673fb8e94308be44460f1db60c0e743
greatbacon <hamiltonjreed@gmail.com>
2021-04-27 20:21:29 -0600

    how did I miss committing this?

--- src/db/Database.ts
+++ src/db/Database.ts
@@ -122,7 +122,7 @@ export class Database {
 			): Promise<Result<{value: Space[]}, {type: 'noSpaceFound'; reason: string}>> => {
 				console.log(`[db] preparring to query for space id: ${spaceId}`);
 				const data = await this.sql<Space[]>`
-				select spaceId, url, mediaType, content from space where spaceId = ${spaceId}
+				select spaceId, url, mediaType, content from spaces where spaceId = ${spaceId}
 				`;
 				console.log('[db] space data', data);
 				if (data.length) {
