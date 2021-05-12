ca077e550918ce5630984f4353cc7d445cabc982
Ryan Atkinson <mail@ryanatkn.com>
2021-05-12 13:32:38 -0600

    fix a type

--- src/spaces/space.ts
+++ src/spaces/space.ts
@@ -1,6 +1,10 @@
+import type {PartialOnly} from '@feltcoop/gro';
+
 export interface Space {
-	space_id?: number;
+	space_id: number;
 	url: string;
 	media_type: string;
 	content: string;
 }
+
+export type SpacePartial = PartialOnly<Space, 'space_id'>;
