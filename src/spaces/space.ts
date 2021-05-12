92afd58e3a337ba696ccc71a4989f7e51fe310ec
Ryan Atkinson <mail@ryanatkn.com>
2021-05-12 14:48:15 -0600

    remove partial

--- src/spaces/space.ts
+++ src/spaces/space.ts
@@ -1,10 +1,6 @@
-import type {OmitStrict} from '@feltcoop/gro';
-
 export interface Space {
 	space_id: number;
 	url: string;
 	media_type: string;
 	content: string;
 }
-
-export type SpacePartial = OmitStrict<Space, 'space_id'>;
