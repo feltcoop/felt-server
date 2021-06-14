82a5abc1b9518534c86c3d99609e973a527decf3
greatbacon <hamiltonjreed@gmail.com>
2021-06-14 15:05:47 -0600

    Fixed runtime issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import node from '@sveltejs/adapter-node';
-import pkg from './package.json';
+import {readFileSync} from 'fs';
+
+const pkg = JSON.parse(readFileSync(new URL('package.json', import.meta.url), 'utf8'));
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+export default {
 	preprocess: typescript(), // TODO mdsvex
 	kit: {
 		adapter: node(),
