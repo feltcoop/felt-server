e8f27c459789b523a0c2c6a2c414747ab739230f
greatbacon <hamiltonjreed@gmail.com>
2021-06-15 22:24:14 -0600

    WIP

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,6 +2,7 @@ import type {GetSession, Handle} from '@sveltejs/kit';
 
 import type {ClientSession} from './session/clientSession.js';
 import type {ClientContext} from './server/ApiServer.js';
+import tryGetSession from 'cookie-session';
 
 // TODO how to import this without breaking everything silently?
 // import {db} from '$lib/db/db.js';
@@ -10,8 +11,19 @@ import type {ClientContext} from './server/ApiServer.js';
 // import {Logger} from '@feltcoop/gro';
 // const log = new Logger(['[hooks]']);
 
-export const getSession: GetSession<ClientContext, ClientSession> = (request) => {
-	console.log('[getSession] authenticated:', request);
+//TODO source this from wherever ApiServer.js does
+const dev = process.env.NODE_ENV !== 'production';
+const TODO_SERVER_COOKIE_KEYS = ['TODO', 'KEY_2_TODO', 'KEY_3_TODO'];
+
+export const getSession: GetSession<ClientContext, ClientSession> = (req) => {
+	tryGetSession({
+		keys: TODO_SERVER_COOKIE_KEYS,
+		maxAge: 1000 * 60 * 60 * 24 * 7 * 6, // 6 weeks
+		secure: !dev, // this makes cookies break in prod unless https! see letsencrypt
+		sameSite: dev ? 'lax' : false,
+		name: 'session_id',
+	})(req, {}, function () {});
+	console.log('[getSession] authenticated:', req.session.name);
 	//console.log('[getSession] authenticated:', !request.context.guest);
 	// const {context} = request;
 	// return context && 'account' in context
