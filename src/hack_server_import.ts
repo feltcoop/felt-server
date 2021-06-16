9f0d8fb6d5da8593fd5e56df9204fbab50c12ca8
Ryan Atkinson <mail@ryanatkn.com>
2021-06-16 09:05:00 -0500

    add another link for context

--- src/hack_server_import.ts
+++ src/hack_server_import.ts
@@ -1,3 +1,4 @@
 // TODO temp hack to workaround Vite prebundling issue:
 // https://github.com/sveltejs/kit/issues/1570
+// https://github.com/vitejs/vite/issues/2579
 export const hack_server_import = (id: string) => import(id);
