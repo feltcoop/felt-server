f0ca87a1d47e33586f5d1c2ee269bca7e52c4e9e
Hamilton Reed <hamiltonjreed@gmail.com>
2021-07-13 16:36:43 -0600

    Add logic for rendering names in chat

--- src/lib/ui/Post_List.svelte
+++ src/lib/ui/Post_List.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import type {Post} from '$lib/posts/post.js';
+	import type {Member} from '$lib/members/member.js';
 
 	export let posts: Post[];
+	export let members: Member[];
 </script>
 
 <ul>
 	{#each posts as post (post.post_id)}
 		<li>
-			<small>{post.actor_id}</small>:
+			<small>{members.find((member) => member.account_id == post.actor_id)?.name}</small>:
 			{post.content}
 		</li>
 	{/each}
