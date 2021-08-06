28012d1c1eaf041743560ea25c0af4cdab83bf45
Hamilton Reed <hamiltonjreed@gmail.com>
2021-08-06 11:31:42 -0600

    Added TODO comments

--- src/lib/personas/persona.ts
+++ src/lib/personas/persona.ts
@@ -8,3 +8,9 @@ export interface Persona_Params {
 	account_id: number;
 	name: string;
 }
+
+//TODO
+//1: Put personas in session response
+//2: Add active persona to cookie info
+//2.5: Render active persona
+//3: Replace account w/ Persona in Community/Spaces/Members/Posts
