d6ea914f03a70bb7e16ae8a2e7be9639e7e01660
Ryan Atkinson <mail@ryanatkn.com>
2021-08-11 14:43:19 -0500

    make nav a permanent onscreen object

--- src/lib/ui/Main_Nav.svelte
+++ src/lib/ui/Main_Nav.svelte
@@ -28,51 +28,57 @@
 	// $: console.log('[Main_Nav] selected_space', selected_space);
 </script>
 
-<div
-	class="main-nav-bg"
-	class:expanded={$ui.expand_main_nav}
-	on:click={() => ($ui.expand_main_nav ? api.toggle_main_nav() : null)}
-/>
-<div class="main-nav" class:expanded={$ui.expand_main_nav}>
-	<div class="header">
-		<!-- TODO how to do this? -->
-		<div class="icon-button button-placeholder" />
-		<button
-			on:click={() => ui.set_main_nav_view('explorer')}
-			class:selected={$ui.main_nav_view === 'explorer'}
-			class="explorer-button"
-		>
-			<img src="/favicon.png" alt="show explorer" />
-		</button>
-		<button
-			on:click={() => ui.set_main_nav_view('account')}
-			class:selected={$ui.main_nav_view === 'account'}
-			class="account-button"
-		>
-			{$data.account.name}
-		</button>
-	</div>
-	{#if $ui.main_nav_view === 'explorer'}
-		<div class="explorer">
-			{#if selected_community}
-				<Community_Nav {members} {communities} {selected_community} />
-				<Space_Nav
-					community={selected_community}
-					spaces={selected_community.spaces}
-					{selected_space}
-					{members}
-				/>
-			{/if}
+{#if $ui.expand_main_nav}
+	<div class="main-nav-bg" on:click={() => ($ui.expand_main_nav ? api.toggle_main_nav() : null)} />
+{/if}
+<div class="main-nav-panel" class:expanded={$ui.expand_main_nav}>
+	<div class="main-nav">
+		<div class="header">
+			<!-- TODO how to do this? -->
+			<div class="icon-button button-placeholder" />
+			<button
+				on:click={() => ui.set_main_nav_view('explorer')}
+				class:selected={$ui.main_nav_view === 'explorer'}
+				class="explorer-button"
+			>
+				<img src="/favicon.png" alt="show explorer" />
+			</button>
+			<button
+				on:click={() => ui.set_main_nav_view('account')}
+				class:selected={$ui.main_nav_view === 'account'}
+				class="account-button"
+			>
+				{$data.account.name}
+			</button>
 		</div>
-	{:else if $ui.main_nav_view === 'account'}
-		<Markup>
-			<Account_Form />
-		</Markup>
-		<Socket_Connection />
-	{/if}
+		{#if $ui.main_nav_view === 'explorer'}
+			<div class="explorer">
+				{#if selected_community}
+					<Community_Nav {members} {communities} {selected_community} />
+					<Space_Nav
+						community={selected_community}
+						spaces={selected_community.spaces}
+						{selected_space}
+						{members}
+					/>
+				{/if}
+			</div>
+		{:else if $ui.main_nav_view === 'account'}
+			<Markup>
+				<Account_Form />
+			</Markup>
+			<Socket_Connection />
+		{/if}
+	</div>
 </div>
 
 <style>
+	.main-nav-panel {
+		width: 0;
+	}
+	.main-nav-panel.expanded {
+		width: var(--column_width_min);
+	}
 	.main-nav {
 		position: relative;
 		z-index: 1;
@@ -86,9 +92,12 @@
 		border-right: var(--border);
 		transform-origin: top left;
 		background-color: hsl(var(--bg_hue), var(--bg_saturation), var(--bg_lightness));
+		transform: translate3d(-100%, 0, 0) scale3d(1, 1, 1);
+		transition: transform var(--transition_duration_xs) ease-out;
 	}
-	.main-nav.expanded {
-		animation: fly-in var(--transition_duration_sm) ease-out;
+	.expanded .main-nav {
+		transform: translate3d(0, 0, 0) scale3d(1, 1, 1);
+		transition-duration: var(--transition_duration_sm);
 	}
 	.main-nav-bg {
 		z-index: 1;
@@ -112,8 +121,8 @@
 		.main-nav-bg {
 			display: block;
 		}
-		.main-nav-bg.expanded {
-			animation: fade-in var(--transition_duration_md) linear;
+		.main-nav-panel.expanded {
+			width: 0;
 		}
 	}
 	.header {
