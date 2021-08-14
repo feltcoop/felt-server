41981f7228b8e6cf1388628f1f31bc5682498bf2
Ryan Atkinson <mail@ryanatkn.com>
2021-08-14 16:14:24 -0500

    shrink forum item icon

--- src/lib/ui/Forum_List_Item.svelte
+++ src/lib/ui/Forum_List_Item.svelte
@@ -26,6 +26,7 @@
 
 <style>
 	li {
+		--icon_size: var(--icon_size_sm);
 		padding: var(--spacing_xs);
 		/* TODO experiment with a border color instead of bg */
 		background-color: hsl(var(--hue), var(--bg_saturation), calc(var(--bg_color_lightness)));
