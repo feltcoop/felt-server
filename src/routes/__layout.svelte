633b17d3fc7002fbe23d46ea3cd96819cbceb402
Ryan Atkinson <mail@ryanatkn.com>
2021-08-20 16:31:56 -0500

    add a todo

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -50,7 +50,7 @@
 	};
 
 	onMount(() => {
-		const socket_url = dev ? `ws://localhost:3001/ws` : `wss://staging.felt.dev/ws`;
+		const socket_url = dev ? `ws://localhost:3001/ws` : `wss://staging.felt.dev/ws`; // TODO env
 		socket.connect(socket_url);
 		return () => {
 			socket.disconnect();
