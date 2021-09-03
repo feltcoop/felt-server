f40fbbc1e3e08fa47c3ccef328a111fb3034d401
Ryan Atkinson <mail@ryanatkn.com>
2021-09-03 11:39:58 -0500

    add dev and prod env files

--- src/lib/server/env.ts
+++ src/lib/server/env.ts
@@ -1,19 +1,26 @@
 import dotenv from 'dotenv';
 import {copyFileSync, existsSync} from 'fs';
 
-const ENV_FILE = '.env'; // any changes to this path must also be made in `svelte.config.js`
-const DEFAULT_ENV_FILE = 'src/infra/.env.default';
-
+const envs: {file: string; defaultFile: string}[] = [
+	// any changes to this path must also be made in `svelte.config.js`
+	{file: '.env', defaultFile: 'src/infra/.env.default'},
+	{file: '.env.development', defaultFile: 'src/infra/.env.development.default'},
+	{file: '.env.production', defaultFile: 'src/infra/.env.production.default'},
+];
 interface Env {
+	// server vars
 	SVELTEKIT_SERVER_HOST: string;
 	API_SERVER_HOST: string;
 
+	// client vars
 	VITE_WEBSOCKET_URL: string;
 
+	// secret vars
+	COOKIE_KEYS: string; // TODO validate this somehow to avoid production security issues
+
+	// deploy vars
 	DEPLOY_IP: string;
 	DEPLOY_USER: string;
-
-	COOKIE_KEYS: string;
 }
 
 let loaded = false;
@@ -21,14 +28,20 @@ let loaded = false;
 export const fromEnv = (key: keyof Env): string => {
 	if (!loaded) {
 		loaded = true;
-		if (!existsSync(ENV_FILE)) {
-			copyFileSync(DEFAULT_ENV_FILE, ENV_FILE);
-		}
-		dotenv.config({path: ENV_FILE});
+		loadEnvs();
 	}
 	const value = process.env[key];
 	if (value === undefined) {
-		throw Error(`Missing env key: ${key}`);
+		throw Error(`Missing environment variable: ${key}`);
 	}
 	return value;
+};
+
+const loadEnvs = () => {
+	for (const env of envs) {
+		if (!existsSync(env.file)) {
+			copyFileSync(env.defaultFile, env.file);
+		}
+		dotenv.config({path: env.file});
+	}
 };
