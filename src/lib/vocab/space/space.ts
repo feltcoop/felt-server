74f1bdf1f5c53665dabd412f263f607793ef7e56
Ryan Atkinson <mail@ryanatkn.com>
2021-09-08 14:15:25 -0500

    fix space type

--- src/lib/vocab/space/space.ts
+++ src/lib/vocab/space/space.ts
@@ -4,13 +4,7 @@ import type {ValidateFunction} from 'ajv';
 
 import {ajv} from '$lib/util/ajv';
 
-export interface Space {
-	space_id: number;
-	name: string;
-	url: string; // TODO should this be computed from the name/community? or is it more useful to cache with both the names of the space and community?
-	media_type: string;
-	content: string;
-}
+export type Space = Static<typeof SpaceSchema>;
 export const SpaceSchema = Type.Object(
 	{
 		space_id: Type.Number(),
