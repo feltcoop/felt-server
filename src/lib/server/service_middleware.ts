58c9175e862ad0fce36b0b1246b5ad4b68eee7cb
Ryan Atkinson <mail@ryanatkn.com>
2021-09-07 19:00:28 -0500

    add some todos

--- src/lib/server/service_middleware.ts
+++ src/lib/server/service_middleware.ts
@@ -4,12 +4,18 @@ import type {ApiServer, Middleware} from '$lib/server/ApiServer.js';
 import type {Service, ServiceParams} from '$lib/server/service';
 
 // Wraps a `Service` in an http `Middleware`
-export const toServiceMiddleware = (server: ApiServer, service: Service): Middleware => {
-	return async (req, res) => {
+export const toServiceMiddleware =
+	(server: ApiServer, service: Service): Middleware =>
+	async (req, res) => {
 		// TODO validate input/output via properties on each `Service`
 		try {
-			const params: ServiceParams = {...req.body, ...req.params};
-			if (!req.account_id) throw Error('TODO unauthorized -- fix type or perform check');
+			const params: ServiceParams = {...req.body, ...req.params, account_id: req.account_id};
+			if (!req.account_id) {
+				// TODO this is duplicating the role of the `authorization_middleware` to avoid mistakes,
+				// but what's the better design here?
+				// Should each service declare if `account_id` is required?
+				return send(res, 401, {reason: 'not logged in'});
+			}
 			const result = await service.handle(server, params, req.account_id);
 			console.log('[service_middleware] result.code', result.code);
 			send(res, result.code, result.data);
@@ -17,4 +23,3 @@ export const toServiceMiddleware = (server: ApiServer, service: Service): Middle
 			send(res, 500, {reason: 'unknown server error'});
 		}
 	};
-};
