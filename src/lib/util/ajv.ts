59f8cc4a39a8ca80773300f00e2f0dba03423cef
Ryan Atkinson <mail@ryanatkn.com>
2021-09-09 15:58:35 -0500

    add notes about ajv extension

--- src/lib/util/ajv.ts
+++ src/lib/util/ajv.ts
@@ -1,7 +1,11 @@
 import Ajv from 'ajv';
 import type {ErrorObject} from 'ajv';
 
-export const ajv = new Ajv().addKeyword('kind').addKeyword('modifier');
+export const ajv = new Ajv()
+	// These are needed so the schemas created by `@sinclair/typebox` don't error --
+	// adding them like this easier than using `Strict`.
+	.addKeyword('kind')
+	.addKeyword('modifier');
 
 // TODO probably misses a bunch of cases
 export const toValidationErrorMessage = (e: ErrorObject): string =>
