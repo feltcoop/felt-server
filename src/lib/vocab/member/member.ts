7b4b2d3eccc9ae58a734050c805f31edef21103b
Ryan Atkinson <mail@ryanatkn.com>
2021-09-13 18:28:29 -0500

    make a note about a hack fix

--- src/lib/vocab/member/member.ts
+++ src/lib/vocab/member/member.ts
@@ -7,7 +7,7 @@ export const MemberSchema = Type.Object(
 	{
 		persona_id: Type.Number(),
 		community_id: Type.Number(),
-		name: Type.Optional(Type.String()), // TODO ??
+		name: Type.Optional(Type.String()), // TODO delete this, is returned in one query but that's now obsolete
 	},
 	{$id: 'Member', additionalProperties: false},
 );
