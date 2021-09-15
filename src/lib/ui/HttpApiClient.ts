047e0766c2e8444343b773dc570cfa41926fc3f9
Ryan Atkinson <mail@ryanatkn.com>
2021-09-15 15:10:51 -0500

    add regexparam to inject values

--- src/lib/ui/HttpApiClient.ts
+++ src/lib/ui/HttpApiClient.ts
@@ -1,5 +1,12 @@
-// this is *not* RESTful as in HATEOS,
-// it's a convenient alternative to the primary websocket API
+// This is *not* RESTful as in HATEOS;
+// it's a convenient alternative to the primary websocket API,
+// and because of its secondary importance it's expected
+// to have significant limitations and quirks
+// compared to a thoughtfully-designed REST API.
+// For future design inspiration see:
+// https://github.com/microsoft/api-guidelines/blob/vNext/Guidelines.md
+
+import {inject} from 'regexparam';
 
 import type {ApiClient} from '$lib/ui/ApiClient';
 import * as servicesMeta from '$lib/server/servicesMeta';
@@ -17,7 +24,7 @@ export const toHttpApiClient = <
 			console.log('[http api client] method, params', method, params);
 			const serviceMeta: ServiceMeta = (servicesMeta as any)[method]; // TODO lighten this dependency, don't need the schemas
 			if (!serviceMeta) throw Error(`Unable to find serviceMeta: ${method}`);
-			const path = method;
+			const path = inject(serviceMeta.route.path, params); // TODO cache this
 			const res = await fetch(path, {
 				method: serviceMeta.route.method.toUpperCase(), // TODO is
 				headers: {'content-type': 'application/json'},
