0235ebc186fcca999d6fa4c4b41be88a1243c690
Ryan Atkinson <mail@ryanatkn.com>
2021-09-17 09:32:51 -0500

    fix result handling

--- src/typecheck.task.ts
+++ src/typecheck.task.ts
@@ -1,10 +1,14 @@
 import type {Task} from '@feltcoop/gro';
-import {spawn} from '@feltcoop/felt/util/process.js';
+import {TaskError} from '@feltcoop/gro';
+import {spawn, printSpawnResult} from '@feltcoop/felt/util/process.js';
 
 export const task: Task = {
-	summary: 'typecheck everything',
+	summary: 'typecheck the project without emitting any files',
 	run: async ({invokeTask}) => {
 		await invokeTask('gro/typecheck');
-		await spawn('npx', ['svelte-check']);
+		const svelteCheckResult = await spawn('npx', ['svelte-check']);
+		if (!svelteCheckResult.ok) {
+			throw new TaskError(`Failed to typecheck. ${printSpawnResult(svelteCheckResult)}`);
+		}
 	},
 };
