1482828d8980a53e6275f21130862543f2406cbd
Ryan Atkinson <mail@ryanatkn.com>
2021-09-17 09:30:47 -0500

    extend typecheck task instead of check

--- src/typecheck.task.ts
+++ src/typecheck.task.ts
@@ -2,9 +2,9 @@ import type {Task} from '@feltcoop/gro';
 import {spawn} from '@feltcoop/felt/util/process.js';
 
 export const task: Task = {
-	summary: 'check that everything is ready to commit',
+	summary: 'typecheck everything',
 	run: async ({invokeTask}) => {
-		await invokeTask('gro/check');
+		await invokeTask('gro/typecheck');
 		await spawn('npx', ['svelte-check']);
 	},
 };
