e1db2249037c519cbd918bedaf22b5234a3a0ca3
Hamilton Reed <hamiltonjreed@gmail.com>
2021-09-23 16:18:06 -0600

    Fix some logic

--- src/lib/vocab/space/spaceServices.ts
+++ src/lib/vocab/space/spaceServices.ts
@@ -130,17 +130,17 @@ export const createSpaceService: Service<
 	perform: async ({server, params}) => {
 		const {db} = server;
 		console.log('[create_space] validating space url uniqueness');
-		const filterByCommunityUrlResult = await db.repos.space.filterByCommunityUrl(
+		const findByCommunityUrlResult = await db.repos.space.findByCommunityUrl(
 			params.community_id,
 			params.url,
 		);
 
-		if (!filterByCommunityUrlResult.ok) {
+		if (!findByCommunityUrlResult.ok) {
 			console.log('[create_space] error validating unique url for new space');
 			return {code: 500, data: {reason: 'error validating unique url for new space'}};
 		}
 
-		if (filterByCommunityUrlResult.value.length !== 0) {
+		if (findByCommunityUrlResult.value) {
 			console.log('[create_space] provided url for space already exists');
 			return {code: 409, data: {reason: 'a space with that url already exists'}};
 		}
