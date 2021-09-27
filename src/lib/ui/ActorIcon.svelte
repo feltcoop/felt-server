f9ef3ba8587645dbd67a24c7e289d4639e434a67
Ryan Atkinson <mail@ryanatkn.com>
2021-09-27 17:55:38 -0500

    ok

--- src/lib/ui/ActorIcon.svelte
+++ src/lib/ui/ActorIcon.svelte
@@ -1,20 +1,16 @@
 <script lang="ts">
 	import type {ActorType} from '$lib/vocab/actor/actor';
 
-	export let icon: string | null = null;
 	export let name: string;
+	export let icon: string | null = null;
 	export let type: ActorType = 'persona';
 </script>
 
 {#if icon}
 	<img class="actor-icon" src={icon} alt="icon for {name}" />
 {:else}
-	<div class="actor-icon">
-		{#if type === 'persona'}
-			⚘
-		{:else}
-			⚝
-		{/if}
+	<div class="actor-icon {type}">
+		{name[0]}
 	</div>
 {/if}
 
@@ -33,5 +29,11 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
+	}
+	div.community {
+		border-radius: calc(var(--icon_size) / 4);
+	}
+	div.community:active {
+		border-radius: calc(var(--icon_size) / 4 + 2px);
 	}
 </style>
