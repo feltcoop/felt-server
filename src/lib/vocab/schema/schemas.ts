4bb9e2302f8e515bb46f693bde73d2d9c81b30fb
Ryan Atkinson <mail@ryanatkn.com>
2021-10-05 15:30:35 -0500

    delete some todos

--- src/lib/vocab/schema/schemas.ts
+++ src/lib/vocab/schema/schemas.ts
@@ -7,11 +7,6 @@ import {MembershipSchema} from '$lib/vocab/membership/membership';
 import {SpaceSchema} from '$lib/vocab/space/space';
 import {FileSchema} from '$lib/vocab/file/file';
 
-// TODO params schemas are duplicated from events -- should use them?
-// or is there no such thing as these params schemas? always contextual to an event?
-
-// TODO what about other schemas like for events?
-
 // TODO generate this file?
 
 export const schemas: AnySchema[] = [
