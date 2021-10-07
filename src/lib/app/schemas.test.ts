eb7c71ac61f4032f2c7745986d064880560428f2
Ryan Atkinson <mail@ryanatkn.com>
2021-10-07 16:59:55 -0500

    reword the test

--- src/lib/app/schemas.test.ts
+++ src/lib/app/schemas.test.ts
@@ -5,19 +5,18 @@ import type {AnySchema} from 'ajv';
 import {schemas} from '$lib/app/schemas';
 import {ID_VOCAB_PREFIX} from '$lib/vocab/util';
 
-const printSchema = (schema: AnySchema) => {
-	return typeof schema === 'boolean'
+const printSchema = (schema: AnySchema) =>
+	typeof schema === 'boolean'
 		? `AnonymousBooleanSchema`
 		: '$id' in schema
 		? schema.$id
 		: 'AnonymousSchema';
-};
 
 /* test__schemas */
 const test__schemas = suite('schemas');
 
 for (const schema of schemas) {
-	test__schemas('ensure entity schema is valid: ' + printSchema(schema), async () => {
+	test__schemas('validate entity schema: ' + printSchema(schema), async () => {
 		t.ok(typeof schema !== 'boolean'); // compared to using `t.type`, this makes TypeScript understand
 		if (!schema.$id) console.log('schema', schema);
 		t.ok(schema.$id);
