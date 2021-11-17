24f5bb79b2e46bbb72846de4a44b7a88808db9f2
Ryan Atkinson <mail@ryanatkn.com>
2021-11-16 18:40:53 -0600

    remove time tracking

--- src/lib/ui/pinger.ts
+++ src/lib/ui/pinger.ts
@@ -4,12 +4,8 @@ export const PINGER_INTERVAL = 300000;
 
 export const createPinger = (dispatch: Dispatch) => {
 	const interval = setInterval(async () => {
-		const time = Date.now();
 		await dispatch('ping');
-		const duration = Date.now() - time;
-		console.log('[ping]', duration + 'ms');
 	}, PINGER_INTERVAL);
-
 	return {
 		close: () => {
 			clearTimeout(interval);
