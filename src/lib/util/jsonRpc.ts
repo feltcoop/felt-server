02527c833be788c3faa9d55b0b297b96302c339e
Ryan Atkinson <mail@ryanatkn.com>
2021-12-08 14:15:17 -0600

    fix type

--- src/lib/util/jsonRpc.ts
+++ src/lib/util/jsonRpc.ts
@@ -3,7 +3,7 @@ export interface JsonRpcRequest<
 	TParams extends Record<TMethod, object> = any, // TODO defaults?
 > {
 	jsonrpc: '2.0';
-	id: string;
+	id: number;
 	method: TMethod;
 	params: TParams[TMethod];
 }
@@ -12,7 +12,7 @@ export interface JsonRpcRequest<
 // or do we want to support multiple kinds of messages?
 export interface JsonRpcResponse<TResult = any> {
 	jsonrpc: '2.0';
-	id: string;
+	id: number;
 	result: TResult;
 }
 
