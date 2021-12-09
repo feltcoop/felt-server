b8d3bc5517023c7dc432accaf5084c9aaf4f856d
Ryan Atkinson <mail@ryanatkn.com>
2021-12-08 23:10:03 -0600

    object

--- src/lib/ui/color.ts
+++ src/lib/ui/color.ts
@@ -1,4 +1,4 @@
 import {toRandomSeeded} from '@feltcoop/felt/util/randomSeeded.js';
 
 // TODO upstream to Felt -- need to figure out patterns with seed and swappable prng
-export const randomHue = (seed?: any): number => toRandomSeeded(seed)() * 360; // TODO random int
+export const randomHue = (seed: Object = ''): number => toRandomSeeded(seed)() * 360; // TODO random int
