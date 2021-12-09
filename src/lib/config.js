8a7bd99b909dafa04edae17c1daf105da11d4200
Hamilton Reed <hamiltonjreed@gmail.com>
2021-12-09 10:01:34 -0700

    extract .env.production

--- src/lib/config.js
+++ src/lib/config.js
@@ -6,6 +6,7 @@
 
 // TODO Replace this at buildtime with a constant so it can be optimized.
 // This probably means supporting `import.meta.env` or `$app/env` in Gro.
+
 const dev = import.meta.env?.DEV ?? process.env.NODE_ENV !== 'production';
 export const VITE_DEPLOY_SERVER_HOST = dev
 	? 'localhost'
