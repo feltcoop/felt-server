c675d44f1beb09efbd545033bed92f2d39607857
Ryan Atkinson <mail@ryanatkn.com>
2021-12-09 13:49:58 -0600

    upgrade felt and improve pane styling

--- src/lib/ui/ContextMenu.svelte
+++ src/lib/ui/ContextMenu.svelte
@@ -1,14 +1,16 @@
 <!-- TODO see usage -- what role does this component play? -->
 
-<div class="context-menu" role="menu" aria-modal tabindex="-1">
+<div class="context-menu pane" role="menu" aria-modal tabindex="-1">
 	<!-- TODO automatically populate with entity commands -->
 	<slot />
 </div>
 
 <style>
 	.context-menu {
-		/* TODO use `.pane` class from Felt when published */
-		background: #fff;
-		border: 2px solid black;
+		/* TODO should this be `pane-light` or something?
+		The `pane` shadow is too heavy because it's designed
+		for contrast against a fullscreen darkened background. */
+		--pane_box_shadow: 0px 2px 10px hsla(0, 100%, 0%, 0.2);
+		border: var(--border);
 	}
 </style>
