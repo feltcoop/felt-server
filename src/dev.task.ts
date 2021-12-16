a4b808fc0ccee83678ce7f1a7fffbf9f0fffbe7e
Ryan Atkinson <mail@ryanatkn.com>
2021-12-16 01:34:33 -0600

    cleanup

--- src/dev.task.ts
+++ src/dev.task.ts
@@ -1,8 +1,7 @@
 import type {Task} from '@feltcoop/gro';
 
-// TODO maybe do this via a config plugin instead of task override?
 export const task: Task = {
-	summary: 'dev',
+	summary: 'start dev server',
 	run: async ({invokeTask}) => {
 		await invokeTask('infra/updateEnv');
 		await invokeTask('gro/dev');
