f2da2ce815d56608a0954c60e2b76f408881d809
Ryan Atkinson <mail@ryanatkn.com>
2022-01-06 22:58:17 -0600

    revert upgrades

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -101,7 +101,7 @@
 
 	// TODO instead of dispatching `select` events on startup, try to initialize with correct values
 	// TODO refactor -- where should this logic go?
-	$: updateStateFromPageParams($page.params, $page.url.searchParams);
+	$: updateStateFromPageParams($page.params, $page.query);
 	const updateStateFromPageParams = (
 		params: {community?: string; space?: string},
 		query: URLSearchParams,
