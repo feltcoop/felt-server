f2178d9ec105f64dacff6c3d3dc8e05b1e947547
Ryan Atkinson <mail@ryanatkn.com>
2022-01-13 02:30:06 -0600

    improve behavior and docs of mutable store

--- src/lib/ui/mutable.ts
+++ src/lib/ui/mutable.ts
@@ -12,6 +12,13 @@ export interface MutableUpdater<T> {
 /**
  * Creates a store representing a mutable `value`.
  * Useful in combination with the Svelte `immutable` compiler flag.
+ * Typical usage mutates `value` inside the `updater` callback and returns nothing,
+ * but the `updater` can return a new reference for `value`.
+ * Calling `update` with no callback triggers a change,
+ * which is useful if `value` is mutated elsewhere.
+ * Returning `undefined` from `updater` keeps the existing `value` reference,
+ * and therefore `undefined` is not a valid `value`,
+ * so prefer `null` over `undefined` to represent empty values.
  * @param value
  * @returns
  */
@@ -25,7 +32,12 @@ export const mutable = <T>(value: T): Mutable<T> => {
 	return {
 		subscribe,
 		update: (updater) => {
-			updater?.(value);
+			if (updater) {
+				const updated = updater(value);
+				if (updated !== undefined && updated !== value) {
+					value = a.value = b.value = updated;
+				}
+			}
 			set((swap = !swap) ? b : a);
 		},
 	};
