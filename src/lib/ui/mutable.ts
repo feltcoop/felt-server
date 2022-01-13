e3e2c6b68721f0cc744f7d51534608d65f6b87b5
Ryan Atkinson <mail@ryanatkn.com>
2022-01-13 14:13:29 -0600

    improve mutable docs

--- src/lib/ui/mutable.ts
+++ src/lib/ui/mutable.ts
@@ -15,13 +15,24 @@ export interface MutableUpdater<T> {
  * Creates a store wrapping a mutable `value`.
  * Useful for values that are expensive to copy, like large `Map`s,
  * in combination with the Svelte `immutable` compiler flag.
+ *
  * Typical usage mutates `value` inside the `updater` callback and returns nothing,
  * but the `updater` can return a new reference for `value`.
- * Calling `update` with no callback triggers a change,
- * which is useful if `value` is mutated elsewhere, but that's usually discouraged.
  * Returning `undefined` from `updater` keeps the existing `value` reference,
  * and therefore `undefined` is not a valid `value`,
  * so prefer `null` over `undefined` to represent empty values.
+ * Calling `update` with no callback triggers a change,
+ * which is useful if `value` is mutated elsewhere, but that's usually discouraged.
+ *
+ * To avoid unnecessary garbage creation,
+ * the implementation swaps between two stable object references.
+ * This may hinder composability with code that
+ * expects immutable references on every store change,
+ * but given that this store is named `mutable`,
+ * it seems acceptable to push this concern downstream for better efficiency in the typical case.
+ * An immutable stream of values can be derived quite easily for those cases that need it:
+ * const someImmutableValue = derived(mutable({}), ($v) => ({value: $v.value}));
+ *
  * @param value
  */
 export const mutable = <T>(value: T): Mutable<T> => {
