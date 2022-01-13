fb343bb87b5e35b86ff328646db5b738ddd6335d
Ryan Atkinson <mail@ryanatkn.com>
2022-01-13 09:26:11 -0600

    fix merge

--- src/lib/ui/SpaceNav.svelte
+++ src/lib/ui/SpaceNav.svelte
@@ -3,9 +3,6 @@
 	import type {Community} from '$lib/vocab/community/community.js';
 	import type {Readable} from 'svelte/store';
 	import SpaceNavItem from '$lib/ui/SpaceNavItem.svelte';
-	import SpaceInput from '$lib/ui/SpaceInput.svelte';
-	import SpaceDelete from '$lib/ui/SpaceDelete.svelte';
-	import MembershipInput from '$lib/ui/MembershipInput.svelte';
 	import type {Persona} from '$lib/vocab/persona/persona.js';
 	import {getApp} from '$lib/ui/app';
 
@@ -20,14 +17,6 @@
 </script>
 
 <nav class="space-nav" use:contextmenu.action={{CommunityContextmenu: {community, persona}}}>
-	<div class="header">
-		<SpaceInput {persona} {community} />
-		<SpaceDelete space={selectedSpace} />
-		{#if $community.name !== $persona.name}
-			<MembershipInput {community} />
-		{/if}
-	</div>
-	<!-- TODO the community url -->
 	{#each spaces as space (space)}
 		<SpaceNavItem {persona} {community} {space} selected={space === selectedSpace} />
 	{/each}
