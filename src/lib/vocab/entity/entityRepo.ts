1acc865b7ede9d4aa15d4f7d0596214e9e70545e
Hamilton Reed <hamiltonjreed@gmail.com>
2022-01-20 08:59:31 -0700

    Rename repo response

--- src/lib/vocab/entity/entityRepo.ts
+++ src/lib/vocab/entity/entityRepo.ts
@@ -9,22 +9,22 @@ export const entityRepo = (db: Database) => ({
 		space_id: number,
 		data: Object,
 	): Promise<Result<{value: Entity}>> => {
-		const response = await db.sql<Entity[]>`
+		const entity = await db.sql<Entity[]>`
 			INSERT INTO entities (actor_id, space_id, data) VALUES (
 				${actor_id},${space_id},${db.sql.json(data)}
 			) RETURNING *
 		`;
 		// console.log('[db] create entity', data);
-		return {ok: true, value: response[0]};
+		return {ok: true, value: entity[0]};
 	},
 	// TODO maybe `EntityQuery`?
 	filterBySpace: async (space_id: number): Promise<Result<{value: Entity[]}>> => {
 		console.log(`[db] preparing to query for space entities: ${space_id}`);
-		const data = await db.sql<Entity[]>`
+		const entities = await db.sql<Entity[]>`
 			SELECT entity_id, data, actor_id, space_id, created, updated 
 			FROM entities WHERE space_id= ${space_id}
 		`;
-		console.log('[db] space entities', data);
-		return {ok: true, value: data};
+		console.log('[db] space entities', entities);
+		return {ok: true, value: entities};
 	},
 });
