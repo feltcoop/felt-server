3e38e9232a655fc97a0c6daa68ff4a5ffd825c7f
Ryan Atkinson <mail@ryanatkn.com>
2022-01-21 09:31:56 -0600

    formatting

--- src/lib/db/migrations/00011-id-references.js
+++ src/lib/db/migrations/00011-id-references.js
@@ -3,19 +3,19 @@ export const up = async (sql) => {
 	// Set persona.account_id foreign key.
 	await sql`
 		ALTER TABLE personas
-    ADD CONSTRAINT personas_account_id_fkey
-      FOREIGN KEY (account_id)
-      REFERENCES accounts (account_id)
-      ON UPDATE CASCADE ON DELETE CASCADE;
+		ADD CONSTRAINT personas_account_id_fkey
+			FOREIGN KEY (account_id)
+			REFERENCES accounts (account_id)
+			ON UPDATE CASCADE ON DELETE CASCADE;
 	`;
 
 	// Make membership cascade on delete community_id.
 	await sql`
-    ALTER TABLE memberships
-    DROP CONSTRAINT memberships_community_id_fkey,
-    ADD CONSTRAINT memberships_community_id_fkey
-      FOREIGN KEY (community_id)
-      REFERENCES communities (community_id)
-      ON UPDATE CASCADE ON DELETE CASCADE;
-  `;
+		ALTER TABLE memberships
+		DROP CONSTRAINT memberships_community_id_fkey,
+		ADD CONSTRAINT memberships_community_id_fkey
+			FOREIGN KEY (community_id)
+			REFERENCES communities (community_id)
+			ON UPDATE CASCADE ON DELETE CASCADE;
+	`;
 };
