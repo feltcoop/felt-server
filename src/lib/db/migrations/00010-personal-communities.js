be7af6e0fe6a4374c6e13434f51ab5083c4bd00d
Ryan Atkinson <mail@ryanatkn.com>
2022-01-20 20:19:29 -0600

    fix migration

--- src/lib/db/migrations/00010-personal-communities.js
+++ src/lib/db/migrations/00010-personal-communities.js
@@ -6,36 +6,36 @@ export const up = async (sql) => {
 	await sql`
 		CREATE TYPE persona_type AS ENUM ('account', 'community');
 	`;
-	// add `type` column to communities
+	// Add `type` column to communities.
 	await sql`
 		ALTER TABLE communities
 			ADD type community_type NOT NULL DEFAULT 'standard';
 	`;
-	// add `type` column to personas
+	// Add `type` column to personas.
 	await sql`
 		ALTER TABLE personas
 			ADD type persona_type NOT NULL DEFAULT 'account';
 	`;
-	// add `community_id` column to personas
+	// Add `community_id` column to personas.
 	await sql`
 		ALTER TABLE personas
 			ADD community_id int REFERENCES communities (community_id) ON UPDATE CASCADE ON DELETE CASCADE;
 	`;
-	// set `community_id` for all 'account' personas
+	// Set `community_id` for all 'account' personas.
 	await sql`
 		UPDATE personas p
 			SET community_id = c.community_id
 			FROM communities c
-			WHERE p.name = c.name AND p.name IN (SELECT c2.name FROM communities c2 JOIN personas p2 ON p2.name = c2.name);
+			WHERE p.name = c.name;
 	`;
-	// create a persona of type 'community' for every community that doesn't have one of the same name
+	// Create a persona of type 'community' for every community that doesn't have one of the same name.
 	await sql`
 		INSERT INTO personas (name, community_id, type)
 			SELECT name, community_id, 'community'
 			FROM communities c2
 			WHERE c2.name NOT IN (SELECT c2.name FROM communities c2 JOIN personas p ON p.name = c2.name);
 	`;
-	// // set the type and `persona_id` reference of each 'personal' community
+	// Set the type of each 'personal' community.
 	await sql`
 		UPDATE communities c
 			SET type = 'personal'
