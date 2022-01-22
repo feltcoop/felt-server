0a597192a01c5257697c569ae3d3d6d1be3eae60
Ryan Atkinson <mail@ryanatkn.com>
2022-01-22 17:04:09 -0600

    add a todo

--- src/lib/ui/contextmenu/ContextmenuItem.svelte
+++ src/lib/ui/contextmenu/ContextmenuItem.svelte
@@ -2,6 +2,10 @@
 	//
 </script>
 
-<button type="button" class="menu-button" on:click>
+<!-- TODO should be <a> ? 
+https://www.w3.org/TR/wai-aria-practices/examples/menu-button/menu-button-links.html
+how to handle the fact that they shouldn't receive focus? disable `Tab`?
+-->
+<button type="button" class="menu-button" role="menu" on:click>
 	<slot />
 </button>
