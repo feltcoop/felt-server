7884400bd4e9eb16586060263ac3d7be5fed6ad4
Ryan Atkinson <mail@ryanatkn.com>
2022-01-27 22:51:18 -0600

    add a comment

--- src/lib/ui/services.ts
+++ src/lib/ui/services.ts
@@ -5,7 +5,8 @@ import {eventInfoByName} from '$lib/app/events';
 export const findHttpService = (name: string): ServiceEventInfo | undefined =>
 	parseServiceEventInfo(eventInfoByName.get(name));
 
-// Websocket services include all of the http services except those that require authorization.
+// Websocket services include all of the http services except those that require authorization,
+// because our server currently disallows unauthenticated websocket connections.
 export const findWebsocketService = (name: string): ServiceEventInfo | undefined => {
 	const serviceEventInfo = parseServiceEventInfo(eventInfoByName.get(name));
 	if (!serviceEventInfo) return undefined;
