73d81de627b91608de082f7a65d3f3123980e584
Ryan Atkinson <mail@ryanatkn.com>
2022-02-14 19:30:47 -0600

    remove a todo

--- src/lib/ui/EntityTable.svelte
+++ src/lib/ui/EntityTable.svelte
@@ -4,9 +4,6 @@
 	import {type Entity} from '$lib/vocab/entity/entity';
 	import EntityCell from '$lib/ui/EntityCell.svelte';
 
-	// TODO clearly display when the thing has changed, and prominently show a save button
-	// along with a "save all" button at the bottom (and for large forms, at the top too)
-
 	export let entity: Readable<Entity>;
 
 	$: properties = Object.keys($entity);
