c15cef82f96135c88c45a9270f283082498b62e4
Ryan Atkinson <mail@ryanatkn.com>
2022-02-15 15:07:02 -0600

    addd contextmenu info to persona avatar

--- src/lib/ui/PersonaAvatar.svelte
+++ src/lib/ui/PersonaAvatar.svelte
@@ -4,10 +4,17 @@
 	import Avatar from '$lib/ui/Avatar.svelte';
 	import {toName, toIcon} from '$lib/vocab/entity/entityHelpers';
 	import {type Persona} from '$lib/vocab/persona/persona';
+	import PersonaContextmenu from '$lib/app/contextmenu/PersonaContextmenu.svelte';
 
 	export let persona: Readable<Persona>;
 	export let showName = true;
 	export let showIcon = true;
 </script>
 
-<Avatar name={toName($persona)} icon={toIcon($persona)} {showName} {showIcon} />
+<Avatar
+	name={toName($persona)}
+	icon={toIcon($persona)}
+	{showName}
+	{showIcon}
+	contextmenuAction={[[PersonaContextmenu, {persona}]]}
+/>
