505753f1cef5498137b659816cc5e7c77099c7c5
Hamilton Reed <hamiltonjreed@gmail.com>
2022-02-16 14:53:43 -0700

    PR notes

--- src/lib/db/migrations/00014-create-ties.js
+++ src/lib/db/migrations/00014-create-ties.js
@@ -2,11 +2,16 @@
 export const up = async (sql) => {
 	await sql`
 	CREATE TABLE IF NOT EXISTS ties (
-		source_id int,
-		destination_id int,
+		source_id int REFERENCES entities (entity_id) ON UPDATE CASCADE ON DELETE CASCADE,
+		destination_id int REFERENCES entities (entity_id) ON UPDATE CASCADE ON DELETE CASCADE,
 		type text,
 		created timestamp NOT NULL DEFAULT now(),
 		updated timestamp		
 	)
+	`;
+
+	await sql`
+		CREATE
+		INDEX ON ties (LOWER(type));
 	`;
 };
