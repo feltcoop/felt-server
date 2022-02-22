ed7d1101eeb9b1ff6fab8a325bb3bfbbc976fa11
greatbacon <4512607+greatbacon@users.noreply.github.com>
2022-02-22 15:17:22 -0700

    Update src/lib/vocab/entity/entityRepo.test.ts
    Co-authored-by: Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>

--- src/lib/vocab/entity/entityRepo.test.ts
+++ src/lib/vocab/entity/entityRepo.test.ts
@@ -21,6 +21,8 @@ test__entityRepo('entites return sorted by created', async ({db}) => {
 	assert.equal(entity0.space_id, entity1.space_id);
 	assert.equal(entity1.space_id, entity2.space_id);
 
+	// Ensure db sort order is shuffled from the insertion order.
+	await db.repos.entity.updateEntityData(entity1.entity_id, entity1.data);
 	const result = await db.repos.entity.filterBySpace(entity0.space_id);
 	assert.ok(result.ok);
 	assert.equal(entity0.entity_id, result.value[0].entity_id);
