cd05b72d84bb25599f39353327c8154d1a1c0275
Hamilton Reed <hamiltonjreed@gmail.com>
2022-03-03 11:36:44 -0700

    Implement and utilize soft delete (i.e. tombstone for entity)

--- src/lib/vocab/entity/entityRepo.ts
+++ src/lib/vocab/entity/entityRepo.ts
@@ -46,7 +46,27 @@ export const entityRepo = (db: Database) =>
 			}
 			return {ok: true, value: result[0]};
 		},
+		//This function is a idempotent soft delete, that leaves behind a Tombstone entity per Activity-Streams spec
 		deleteById: async (
+			entity_id: number,
+		): Promise<Result<{value: any[]}, {type: 'deletion_error'} & ErrorResponse>> => {
+			console.log('[entityRepo] deleting space :', entity_id);
+			const data = await db.sql<any[]>`
+				UPDATE entities
+				SET data = jsonb_build_object('type','Tombstone','formerType',data->'type','deleted',NOW())
+				WHERE entity_id=${entity_id} AND data->>'type' != 'Tombstone';
+			`;
+			if (data.count !== 1) {
+				return {
+					ok: false,
+					type: 'deletion_error',
+					message: 'failed to delete entity',
+				};
+			}
+			return {ok: true, value: data};
+		},
+		//This function actually deletes the record in the DB
+		hardDeleteById: async (
 			entity_id: number,
 		): Promise<Result<{value: any[]}, {type: 'deletion_error'} & ErrorResponse>> => {
 			console.log('[entityRepo] deleting space :', entity_id);
@@ -57,7 +77,7 @@ export const entityRepo = (db: Database) =>
 				return {
 					ok: false,
 					type: 'deletion_error',
-					message: 'failed to delete entity',
+					message: 'failed to hard delete entity',
 				};
 			}
 			return {ok: true, value: data};
