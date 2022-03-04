79570d909fc1d7b988a9381c143caf51db340f75
Ryan Atkinson <mail@ryanatkn.com>
2022-03-04 16:08:06 -0600

    trace

--- src/lib/vocab/tie/tieRepo.ts
+++ src/lib/vocab/tie/tieRepo.ts
@@ -18,25 +18,25 @@ export const tieRepo = (db: Database) =>
 				${source_id},${dest_id},${type}
 			) RETURNING source_id,dest_id,type,created
 		`;
-			// log.info('create entity', data);
+			// log.trace('create entity', data);
 			return {ok: true, value: tie[0]};
 		},
 		//TODO once the system is ported from a 1:1 entity:space relation to the Directory structure
 		// a query like the following could be used
 		filterBySpace: async (space_id: number): Promise<Result<{value: Tie[]}>> => {
-			log.info(`preparing to query for space ties: ${space_id}`);
+			log.trace(`preparing to query for space ties: ${space_id}`);
 			const ties = await db.sql<Tie[]>`
 			SELECT DISTINCT source_id,dest_id,type,created 
 			FROM ties t 
 			JOIN (SELECT entity_id FROM entities WHERE space_id=${space_id}) as e 
 			ON e.entity_id = t.source_id OR e.entity_id = t.dest_id;
 			`;
-			log.info('space ties', ties);
+			log.trace('space ties', ties);
 			return {ok: true, value: ties};
 		},
 
 		filterBySourceId: async (directory_id: number): Promise<Result<{value: Tie[]}>> => {
-			log.info(`preparing to walk graph starting with directory: ${directory_id}`);
+			log.trace(`preparing to walk graph starting with directory: ${directory_id}`);
 			const ties = await db.sql<Tie[]>`
 			WITH RECURSIVE paths (source_id, dest_id, type, created, path) AS (
         	SELECT t.source_id, t.dest_id, t.type, t.created, ARRAY[t.source_id, t.dest_id]
@@ -49,7 +49,7 @@ export const tieRepo = (db: Database) =>
 				)
 			SELECT DISTINCT source_id, dest_id, type, created FROM paths;
 			`;
-			log.info('directory ties', ties);
+			log.trace('directory ties', ties);
 			return {ok: true, value: ties};
 		},
 	} as const);
