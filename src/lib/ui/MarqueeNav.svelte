c124394b5d9d0f1a7bff8620b57d83234f80b296
Ryan Atkinson <mail@ryanatkn.com>
2022-03-09 20:56:58 -0600

    move a line

--- src/lib/ui/MarqueeNav.svelte
+++ src/lib/ui/MarqueeNav.svelte
@@ -6,9 +6,9 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding-left: var(--spacing_lg);
 		height: var(--navbar_size);
 		background-color: var(--tint_dark_1);
 		padding-right: var(--navbar_size); /* placeholder for button, which is rendered elsewhere */
+		padding-left: var(--spacing_lg);
 	}
 </style>
