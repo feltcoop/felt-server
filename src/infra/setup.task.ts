5b31f366559394afd4aee1829461515596bd03ff
Ryan Atkinson <mail@ryanatkn.com>
2022-04-11 21:35:51 -0500

    improve setup task

--- src/infra/setup.task.ts
+++ src/infra/setup.task.ts
@@ -3,82 +3,99 @@ import {spawn} from '@feltcoop/felt/util/process.js';
 
 import {fromEnv} from '$lib/server/env';
 import {toNginxConfig} from './nginxConfig';
+import {toLogSequence} from './helpers';
 
 export const task: Task = {
 	summary: 'setup a clean server to prepare for a felt-server deploy',
 	production: true,
-	run: async ({fs}) => {
+	run: async ({log, fs}) => {
 		const DEPLOY_IP = fromEnv('DEPLOY_IP');
 		const DEPLOY_USER = fromEnv('DEPLOY_USER');
 		const VITE_DEPLOY_SERVER_HOST = fromEnv('VITE_DEPLOY_SERVER_HOST');
 		const EMAIL_ADDRESS = fromEnv('EMAIL_ADDRESS');
 
+		// TODO BLOCK do this without the hack
+		const {API_SERVER_HOST_PROD, SVELTEKIT_SERVER_HOST} = await import('../lib/config.js');
+
+		const REMOTE_NGINX_CONFIG_PATH = '/etc/nginx/sites-available/felt-server.conf';
+		const nginxConfig = toNginxConfig(
+			fromEnv('VITE_DEPLOY_SERVER_HOST'),
+			API_SERVER_HOST_PROD,
+			SVELTEKIT_SERVER_HOST,
+		);
+
+		const logSequence = toLogSequence();
+
 		// TODO BLOCK how to detect and bail? maybe put hash
-		// ~/.felt-initialized -- this file gets checked to see if the script has been run
+		// ~/.felt_setup -- this file gets checked to see if the script has been run
+		// ~/.felt_deploy
+		// ~/.felt_restart
 
 		const deployLogin = `${DEPLOY_USER}@${DEPLOY_IP}`;
-		//TODO set up initial user accounts & directory system
-		//TODO break up commands for better error handling
-		//Install initial tools for Node ecosystem
-		await spawn('ssh', [
-			deployLogin,
-			`apt update;
-			apt install unzip;			
-		  curl -fsSL https://fnm.vercel.app/install | bash;`,
-		]);
-		//Splitting these tasks here let's fnm get picked up from the bash profile
-		// TODO BLOCK npm
-		await spawn('ssh', [
-			deployLogin,
-			`/root/.fnm/fnm install 16;
-			apt install npm;
-			npm install pm2@latest -g`,
-		]);
-		//This chunk manages the NGINX & HTTPS config
-		await spawn('ssh', [
-			deployLogin,
-			`apt install -y certbot nginx python3-certbot-nginx;
-			systemctl start nginx;
-			sudo unlink /etc/nginx/sites-enabled/default;`,
-		]);
-		// TODO BLOCK do this without the hack
-		const {API_SERVER_HOST_PROD, SVELTEKIT_SERVER_HOST} = await import('../lib/config');
-		await fs.writeFile(
-			'src/infra/felt-server.conf',
-			toNginxConfig(
-				fromEnv('VITE_DEPLOY_SERVER_HOST'),
-				API_SERVER_HOST_PROD,
-				SVELTEKIT_SERVER_HOST,
-			),
-		);
-		await spawn('cat', [`src/infra/felt-server.conf`]);
-		await spawn('scp', [
-			`src/infra/felt-server.conf`,
-			`${deployLogin}:/etc/nginx/sites-available/felt-server.conf`,
-		]);
-		// //TODO stuff is still a little unstable arount this
-		// //Make sure youre DNS records are set up and configured first
-		await spawn('ssh', [
-			deployLogin,
-			`ln -s /etc/nginx/sites-available/felt-server.conf /etc/nginx/sites-enabled/felt-server.conf;
-			certbot --nginx --non-interactive --agree-tos --email ${EMAIL_ADDRESS} -d ${VITE_DEPLOY_SERVER_HOST}
-			systemctl restart nginx.service;
-			`,
-		]);
 
-		//TODO install postgres & initialize database
-		//see lib/db/README for more details
-		// TODO BLOCK do this
-		// sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-		// curl -L https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-		// sudo apt update
-		// sudo apt install -y postgresql
-		// TODO BLOCK configure
-		// sudo -u postgres psql
-		// # in psql:
-		// # postgres=#
-		// create database felt; # notice the semicolon
-		// \password
-		// <enter "password">
+		//TODO set up initial user accounts & directory system
+
+		//Install initial tools for Node ecosystem
+		const result = await spawn('ssh', [
+			deployLogin,
+			logSequence('Setting up server instance...') +
+				// Configure shell behavior:
+				// 	-e — exit the script if any command returns a nonzero exit code
+				// 	-u — throw an error if nonexistent variables are accessed
+				'set -eu;' +
+				// Update and upgrade apt
+				logSequence('Updating apt...') +
+				`apt update;
+				apt upgrade -y;` +
+				// Install fnm:
+				logSequence('Installing fnm...') +
+				`apt install -y unzip;
+				curl -fsSL https://fnm.vercel.app/install | bash;
+				export PATH=/root/.fnm:$PATH;
+				eval "\`fnm env\`";` +
+				// Install Node:
+				logSequence('Installing Node...') +
+				`fnm install 16;
+				fnm use 16;
+				fnm default 16;
+				npm i -g npm@latest;` +
+				// Install Node tools:
+				logSequence('Installing pm2 and gro...') +
+				`npm i -g pm2 @feltcoop/gro;` +
+				// Install nginx & certbot for HTTPS:
+				logSequence('Installing nginx and certbot...') +
+				`apt install -y nginx certbot python3-certbot-nginx;
+				systemctl start nginx;
+				sudo unlink /etc/nginx/sites-enabled/default;` +
+				// Create the nginx config:
+				logSequence('Creating nginx config...') +
+				`touch ${REMOTE_NGINX_CONFIG_PATH};
+				echo '${nginxConfig}' >> ${REMOTE_NGINX_CONFIG_PATH};
+				cat ${REMOTE_NGINX_CONFIG_PATH};` +
+				//Make sure your DNS records are set up and configured first
+				//TODO stuff is still a little unstable arount this
+				logSequence('Enabling HTTPS with cerbot and nginx...') +
+				`ln -s ${REMOTE_NGINX_CONFIG_PATH} /etc/nginx/sites-enabled/felt-server.conf;
+				certbot --nginx --non-interactive --agree-tos --email ${EMAIL_ADDRESS} -d ${VITE_DEPLOY_SERVER_HOST};
+				systemctl restart nginx.service;` +
+				// Install Postgres:
+				// More details at src/lib/db/README.md and https://www.postgresql.org/download/linux/ubuntu/
+				logSequence('Installing Postgres...') +
+				`sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list;'
+				curl -L https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -;
+				sudo apt update;
+				sudo apt install -y postgresql;`,
+			// TODO BLOCK initialize the database
+			// sudo -u postgres psql
+			// # in psql:
+			// # postgres=#
+			// create database felt; # notice the semicolon
+			// \password
+			// <enter "password">
+		]);
+		if (!result.ok) {
+			log.error('spawn failed with signal', result.signal);
+			throw Error('Failed setup task');
+		}
 	},
 };
