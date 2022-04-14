1e10e2721af21dd5498164a6654b652013a9c7b2
Ryan Atkinson <mail@ryanatkn.com>
2022-04-14 00:58:32 -0500

    rearrange

--- src/lib/util/deserialize.ts
+++ src/lib/util/deserialize.ts
@@ -2,6 +2,20 @@ import {traverse} from '@feltcoop/felt/util/object.js';
 
 // TODO where does this belong? vocab utils?
 
+export interface Deserialize {
+	(value: any): void;
+}
+
+export const deserialize =
+	(deserializers: Deserializers): Deserialize =>
+	(value) => {
+		traverse(value, (key, value, obj) => {
+			if (deserializers.has(key)) {
+				obj[key] = deserializers.get(key)!(value);
+			}
+		});
+	};
+
 export const deserializeDate = (v: string): Date => new Date(v);
 
 export type Deserializers = Map<string, (v: any) => any>;
@@ -10,17 +24,3 @@ export const deserializers: Deserializers = new Map([
 	['created', deserializeDate],
 	['updated', deserializeDate],
 ]);
-
-export interface DeserializeProperties {
-	(value: any): void;
-}
-
-export const deserialize =
-	(deserializers: Deserializers): DeserializeProperties =>
-	(value) => {
-		traverse(value, (key, value, obj) => {
-			if (deserializers.has(key)) {
-				obj[key] = deserializers.get(key)!(value);
-			}
-		});
-	};
