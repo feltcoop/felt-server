1d465412c090c95a150ea24d1959164faf4118ff
Ryan Atkinson <mail@ryanatkn.com>
2022-04-14 03:04:57 -0500

    add a todo

--- src/lib/ui/SvastView.svelte
+++ src/lib/ui/SvastView.svelte
@@ -11,11 +11,17 @@
 	export let view: ViewNode;
 
 	// TODO `toComponentViewProps` and `toElementViewProps`? or expand this API?
-	// Maybe `toViewProps` with optional whitelist set of property names?
-	// Needs more flexibility than that -- e.g. to remove external links but preserve internal ones
+	// Maybe `toViewProps` with optional allowlist of property names?
+	// A simple allowlist is not enough: it needs the flexibility to, for example,
+	// remove external links but preserve internal ones.
+	// But should that be done upstream, before the properties are saved in the `view`,
+	// through a `sanitize` helper that converts a parsed SVAST to a sanitized one?
+	// Then this component wouldn't have to concern itself with security.
 	$: props = toViewProps(view) || EMPTY_OBJECT;
 </script>
 
+<!-- TODO is using keyed each blocks ideal here? maybe create an id for each node? -->
+<!-- TODO should this render a fallback for unknown components? perhaps with a button to select a valid one? -->
 {#if view.type === 'svelteComponent'}{#if view.tagName in components}<svelte:component
 			this={components[view.tagName]}
 			{...props}
