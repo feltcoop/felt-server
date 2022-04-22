3dcc69008a8d8475efabe8135472deb1f051e491
Hamilton Reed <hamiltonjreed@gmail.com>
2022-04-22 12:25:51 -0600

    WIP - issue with Double loading

--- src/lib/ui/view/Home.svelte
+++ src/lib/ui/view/Home.svelte
@@ -1,17 +1,67 @@
 <script lang="ts">
+	import {browser} from '$app/env';
 	import {getApp} from '$lib/ui/app';
 	import {getViewContext} from '$lib/vocab/view/view';
 	import Forum from '$lib/ui/view/Forum.svelte';
 	import PersonaAvatar from '$lib/ui/PersonaAvatar.svelte';
+	import {get} from 'svelte/store';
 
 	const viewContext = getViewContext();
-	$: ({community} = $viewContext);
+	$: ({community, space, persona} = $viewContext);
 
 	const {
 		ui: {personasByCommunityId},
+		socket,
+		dispatch,
 	} = getApp();
 
+	const DEFAULT_RULES = `<ol>
+				<li>
+					No tolerance for any sort of hate and discrimination such as racism, sexism, ableism,
+					transphobia, etc.
+				</li>
+				<li>No spamming</li>
+				<li>If there is a conflict, please report issues to community leaders</li>
+			</ol>`;
+
+	const DEFAULT_NORMS = `<p>
+				some thoughts about our community’s vibes that aren’t rules, but still worth thinking about
+			</p>
+			<ol>
+				<li>We welcome nerdiness :)</li>
+				<li>We strive to learn from each other.</li>
+				<li>We encourage everyone to participate in moderation.</li>
+			</ol>`;
+
 	$: communityPersonas = $personasByCommunityId.get($community.community_id)!;
+	$: shouldLoadEntities = browser && $socket.open;
+	$: entities = shouldLoadEntities ? dispatch.QueryEntities({space_id: $space.space_id}) : null;
+
+	const createEntity = async (text: string, name: string) => {
+		const content = text.trim(); // TODO parse to trim? regularize step?
+
+		if (!content) return;
+		await dispatch.CreateEntity({
+			space_id: $space.space_id,
+			data: {type: 'Article', content, name},
+			actor_id: $persona.persona_id,
+			source_id: $space.directory_id,
+		});
+	};
+
+	$: console.log('should load', shouldLoadEntities);
+	$: console.log('checking entities');
+	$: if ($entities) {
+		console.log('array exists', $entities);
+		const result = $entities.filter(
+			(e) => get(e).data.name === 'rules' || get(e).data.name === 'norms',
+		);
+		console.log('array with rules &  norms', result);
+		// if (result.length === 0) {
+		// 	void createEntity(DEFAULT_RULES, 'rules');
+		// 	void createEntity(DEFAULT_NORMS, 'norms');
+		// }
+	}
 </script>
 
 <div class="home">
@@ -32,27 +82,11 @@
 	<section class="rules-and-norms">
 		<div class="rules markup panel-inset">
 			<h4>rules</h4>
-			<ol>
-				<li>
-					No tolerance for any sort of hate and discrimination such as racism, sexism, ableism,
-					transphobia, etc.
-				</li>
-				<li>No spams</li>
-				<li>If there is a conflict, we have a conflict resolution process where...</li>
-			</ol>
+			{@html DEFAULT_RULES}
 		</div>
 		<div class="norms markup panel-inset">
 			<h4>norms</h4>
-
-			<p>
-				some thoughts about our community’s vibes that aren’t rules, but still worth thinking about
-			</p>
-
-			<ol>
-				<li>We welcome nerdiness :)</li>
-				<li>We strive to learn from each other.</li>
-				<li>We encourage everyone to participate in moderation.</li>
-			</ol>
+			{@html DEFAULT_NORMS}
 		</div>
 	</section>
 	<section class="roles">
