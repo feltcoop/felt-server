623cf7e983e8cb7c32cdc73d42ce5b1fdc833838
Ryan Atkinson <mail@ryanatkn.com>
2022-04-26 18:05:41 -0500

    todos

--- src/lib/app/contextmenu/ActingPersonaContextmenu.svelte
+++ src/lib/app/contextmenu/ActingPersonaContextmenu.svelte
@@ -6,8 +6,16 @@
 	import type {Persona} from '$lib/vocab/persona/persona';
 	import ContextmenuEntry from '$lib/ui/contextmenu/ContextmenuEntry.svelte';
 	import ContextmenuSubmenu from '$lib/ui/contextmenu/ContextmenuSubmenu.svelte';
-	import CommunityInput from '$lib/ui/CommunityInput.svelte';
-	import ManageMembershipForm from '$lib/ui/ManageMembershipForm.svelte';
+
+	// TODO BLOCK previously these were just imported and forwarded as event props,
+	// but making things serializable means we can't do that.
+	// The problem with registering them globally as `components` is they'll bloat the default JS payload.
+	// I can see two solutions right now:
+	// - dynamic imports in the dialog opener
+	// - register them here, like `registerComponent(CommunityInput)`,
+	// so they'll be available in `components` because every dispatching module is responsible for registering what they use
+	// import CommunityInput from '$lib/ui/CommunityInput.svelte';
+	// import ManageMembershipForm from '$lib/ui/ManageMembershipForm.svelte';
 
 	const {dispatch} = getApp();
 
@@ -22,14 +30,17 @@
 		<ContextmenuEntry
 			action={() =>
 				dispatch.OpenDialog({
-					Component: CommunityInput,
+					Component: 'CommunityInput',
+					// TODO how to handle `done` with serializable props?
+					// maybe we need to register it here in the component? see above too.
+					// `registerDialog(CommunityInput, {done: () => ...})`
 					props: {persona, done: () => dispatch.CloseDialog()},
 					dialogProps: {layout: 'page'},
 				})}
 		>
 			<span class="title">Create Community</span>
 		</ContextmenuEntry>
-		<ContextmenuEntry action={() => dispatch.OpenDialog({Component: ManageMembershipForm})}>
+		<ContextmenuEntry action={() => dispatch.OpenDialog({Component: 'ManageMembershipForm'})}>
 			<span class="title">Manage Memberships</span>
 		</ContextmenuEntry>
 	</svelte:fragment>
