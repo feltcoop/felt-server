97f02e5f8110a286d1d29e583582a9bd97d06b50
Ryan Atkinson <mail@ryanatkn.com>
2022-04-28 12:24:21 -0500

    pluralize randomizer

--- src/lib/util/randomEventParams.ts
+++ src/lib/util/randomEventParams.ts
@@ -126,8 +126,10 @@ export const randomEventParams = async (
 			};
 		}
 		case 'EraseEntities': {
+			const entity1 = await random.entity(persona, account, community, space);
+			const entity2 = await random.entity(persona, account, community, space);
 			return {
-				entity_id: (await random.entity(persona, account, community, space)).entity.entity_id,
+				entity_ids: [entity1.entity.entity_id, entity2.entity.entity_id],
 			};
 		}
 		case 'DeleteEntities': {
