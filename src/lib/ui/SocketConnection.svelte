bbf5949f4df949e3779531835f1f30a7964fbb1a
Ryan Atkinson <mail@ryanatkn.com>
2022-06-01 16:09:18 -0500

    refactor

--- src/lib/ui/SocketConnection.svelte
+++ src/lib/ui/SocketConnection.svelte
@@ -1,32 +1,25 @@
 <script lang="ts">
-	import {onMount} from 'svelte';
+	import {onDestroy} from 'svelte';
 	import {session} from '$app/stores';
+	import {browser} from '$app/env';
 
 	import type {SocketStore} from '$lib/ui/socket';
 
 	export let socket: SocketStore;
 	export let url: string;
 
-	$: guest = $session.guest;
+	$: ({guest} = $session);
 
-	let mounted = false;
-
-	onMount(() => {
-		mounted = true;
-		return () => {
-			socket.disconnect();
-		};
+	onDestroy(() => {
+		socket.disconnect();
 	});
 
 	// Keep the socket connected when logged in, and disconnect when logged out.
-	$: if (mounted) {
-		console.log(':CONNECTING');
+	$: if (browser) {
 		if (guest) {
 			socket.disconnect();
 		} else {
 			socket.connect(url);
 		}
-	} else {
-		console.log('NOT YET MOUNTED');
 	}
 </script>
