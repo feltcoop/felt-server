8fe842032e49088f414df6ca1f262c0fd77b3083
Hamilton Reed <hamilton@felt.social>
2022-06-01 14:09:06 -0600

    Move spaces/directories query into spacerepo

--- src/lib/session/SessionRepo.ts
+++ src/lib/session/SessionRepo.ts
@@ -3,8 +3,6 @@ import {Logger} from '@feltcoop/felt/util/log.js';
 import {blue, gray} from 'kleur/colors';
 
 import {PostgresRepo} from '$lib/db/PostgresRepo';
-import type {Space} from '$lib/vocab/space/space';
-import type {Entity} from '$lib/vocab/entity/entity';
 
 const log = new Logger(gray('[') + blue('SessionRepo') + gray(']'));
 
@@ -15,34 +13,29 @@ export class SessionRepo extends PostgresRepo {
 		if (!accountResult.ok) return accountResult;
 		const account = accountResult.value;
 
-		const data = await this.db.sql<Array<{space: Space; entity: Entity}>>`
-		SELECT json_build_object('space_id',s.space_id,'name',s.name,'url',s.url,'icon',s.icon,'view',s.view,'created',s.created,'updated',s.updated,'community_id',s.community_id,'directory_id',s.directory_id) space, 
-		json_build_object('entity_id',e.entity_id,'data',e.data,'persona_id',e.persona_id,'created',e.created,'updated',e.updated) entity  
-		FROM entities e JOIN (			     
-		SELECT s.space_id, s.name, s.url, s.icon, s.view, s.created,s.updated, s.community_id, s.directory_id
-						FROM spaces s JOIN (
-							SELECT DISTINCT m.community_id FROM personas p
-							JOIN memberships m ON p.persona_id=m.persona_id AND p.account_id=${account.account_id}
-						) apc
-						ON s.community_id=apc.community_id
-		) s ON e.entity_id=s.directory_id;
-			`;
-
-		const spaces = data.map((r) => r.space);
-		const directories = data.map((r) => r.entity);
-
 		// TODO make this a single query
-		const [sessionPersonasResult, communitiesResult, membershipsResult, personasResult] =
-			await Promise.all([
-				this.db.repos.persona.filterByAccount(account.account_id),
-				this.db.repos.community.filterByAccount(account.account_id),
-				this.db.repos.membership.filterByAccount(account.account_id),
-				this.db.repos.persona.getAll(), //TODO don't getAll
-			]);
+		const [
+			spacesResult,
+			sessionPersonasResult,
+			communitiesResult,
+			membershipsResult,
+			personasResult,
+		] = await Promise.all([
+			this.db.repos.space.filterByAccountWithDirectories(account.account_id),
+			this.db.repos.persona.filterByAccount(account.account_id),
+			this.db.repos.community.filterByAccount(account.account_id),
+			this.db.repos.membership.filterByAccount(account.account_id),
+			this.db.repos.persona.getAll(), //TODO don't getAll
+		]);
+		if (!spacesResult.ok) return spacesResult;
 		if (!sessionPersonasResult.ok) return sessionPersonasResult;
 		if (!communitiesResult.ok) return communitiesResult;
 		if (!membershipsResult.ok) return membershipsResult;
 		if (!personasResult.ok) return personasResult;
+
+		const spaces = spacesResult.value.map((r) => r.space);
+		const directories = spacesResult.value.map((r) => r.entity);
+
 		return {
 			ok: true,
 			value: {
