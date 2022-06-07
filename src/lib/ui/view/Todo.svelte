2908b472afecac2a52975297d1d858c8e40e3d24
Hamilton Reed <hamilton@felt.social>
2022-06-07 17:21:54 -0600

    Got reactive ties!

--- src/lib/ui/view/Todo.svelte
+++ src/lib/ui/view/Todo.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import {browser} from '$app/env';
 	import PendingAnimation from '@feltcoop/felt/ui/PendingAnimation.svelte';
-	import type {Readable} from '@feltcoop/svelte-gettable-stores';
 
 	import TodoItems from '$lib/ui/TodoItems.svelte';
 	import {getApp} from '$lib/ui/app';
-	import type {Tie} from '$lib/vocab/tie/tie';
 	import type {Entity} from '$lib/vocab/entity/entity';
 	import {getViewContext} from '$lib/vocab/view/view';
 	import EntityInput from '$lib/ui/EntityInput.svelte';
@@ -19,22 +17,9 @@
 	$: entities = shouldLoadEntities
 		? dispatch.QueryEntities({source_id: $space.directory_id})
 		: null;
-	$: tiesResult = shouldLoadEntities ? dispatch.ReadTies({source_id: $space.directory_id}) : null;
-	let ties: Tie[] | undefined;
 	let text = '';
 
-	//TODO move this call to the UI to get arch & caching
-	$: void tiesResult?.then((data) => {
-		if (data.ok) {
-			ties = data.value.ties;
-		}
-	});
-
-	$: itemsByEntity = $entities && ties ? toItemsByEntity($entities, ties) : null;
-
-	let entityById: Map<number, Readable<Entity>> | null = null;
-	$: entityById = $entities && new Map($entities.map((e) => [e.get().entity_id, e]));
-
+	//TODO this should be readable
 	let selectedList: Entity | null = null;
 	const selectList = (list: Entity) => {
 		if (list.data.type !== 'Collection') return;
@@ -43,25 +28,6 @@
 		} else {
 			selectedList = list;
 		}
-	};
-
-	//TODO do caching here
-	const toItemsByEntity = (
-		entities: Array<Readable<Entity>>,
-		ties: Tie[],
-	): Map<Readable<Entity>, Array<Readable<Entity>>> => {
-		const map: Map<Readable<Entity>, Array<Readable<Entity>>> = new Map();
-		for (const tie of ties) {
-			if (tie.type !== 'HasItem') continue;
-			const source = entities.find((e) => e.get().entity_id === tie.source_id)!;
-			const dest = entities.find((e) => e.get().entity_id === tie.dest_id)!;
-			let items = map.get(source);
-			if (!items) {
-				map.set(source, (items = []));
-			}
-			items.push(dest);
-		}
-		return map;
 	};
 
 	const createEntity = async () => {
@@ -87,36 +53,28 @@
 	};
 
 	const clearDone = async () => {
-		if (!selectedList) return;
-		const entityList = entityById!.get(selectedList.entity_id);
-		const items = itemsByEntity?.get(entityList!);
-		if (items) {
-			const doneItems = items.filter((i) => i.get().data.checked === true);
-			if (doneItems.length > 0) {
-				const entity_ids = doneItems.map((i) => i.get().entity_id);
-				await dispatch.DeleteEntities({entity_ids});
-				await dispatch.UpdateEntity({
-					data: null,
-					entity_id: $space.directory_id,
-				});
-			}
-		}
+		// if (!selectedList) return;
+		// const entityList = entityById.get(selectedList.entity_id);
+		// const items = itemsByEntity?.get(entityList!);
+		// if (items) {
+		// 	const doneItems = items.filter((i) => i.get().data.checked === true);
+		// 	if (doneItems.length > 0) {
+		// 		const entity_ids = doneItems.map((i) => i.get().entity_id);
+		// 		await dispatch.DeleteEntities({entity_ids});
+		// 		await dispatch.UpdateEntity({
+		// 			data: null,
+		// 			entity_id: $space.directory_id,
+		// 		});
+		// 	}
+		// }
 	};
 </script>
 
 <div class="room">
 	<div class="entities">
 		<!-- TODO handle failures here-->
-		{#if entities && ties && itemsByEntity && entityById}
-			<TodoItems
-				{entities}
-				{space}
-				{ties}
-				{itemsByEntity}
-				{entityById}
-				{selectedList}
-				{selectList}
-			/>
+		{#if entities}
+			<TodoItems {entities} {space} {selectedList} {selectList} />
 			<button
 				on:click={() =>
 					dispatch.OpenDialog({
