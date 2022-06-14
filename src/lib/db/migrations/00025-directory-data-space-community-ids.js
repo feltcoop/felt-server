e660fc3cfc571bb942191e22dc6ff50e36bf92fb
Ryan Atkinson <mail@ryanatkn.com>
2022-06-14 12:19:34 -0500

    implement without migration

---
+++ src/lib/db/migrations/00025-directory-data-space-community-ids.js
@@ -0,0 +1,6 @@
+/** @param {import('postgres').Sql<any>} sql */
+export const up = async (sql) => {
+	// TODO BLOCK for every `space.directory_id` set `entity.data.community_id` and `entity.data.space_id`
+	// await sql`
+	// `;
+};
--- src/lib/vocab/entity/entityData.ts
+++ src/lib/vocab/entity/entityData.ts
@@ -9,6 +9,8 @@ export interface BaseEntityData {
 	content?: string;
 	name?: string;
 	checked?: boolean;
+	community_id?: number; // populated for directories, `undefined` for all other entities
+	space_id?: number; // populated for directories, `undefined` for all other entities
 }
 
 export interface NoteEntityData extends BaseEntityData {
