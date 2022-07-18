44996cf909c737c3537ecc89071d7956ebb36287
Ryan Atkinson <mail@ryanatkn.com>
2022-07-18 14:17:39 -0500

    make broadcast logging more legible

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -89,6 +89,7 @@
 		log.trace('app', app);
 	}
 
+	// TODO might need to synchronously dispatch on component initialization and then avoid the second re-init
 	$: dispatch.SetSession({session: $session});
 
 	const {mobile, layout, contextmenu, dialogs, account, sessionPersonas, personaSelection} = ui;
