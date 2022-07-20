bad5f927d9e83eb98d68a9e21d6dcd1d0a1a9bad
Ryan Atkinson <mail@ryanatkn.com>
2022-07-19 19:15:27 -0500

    add target for external links

--- src/lib/ui/Link.svelte
+++ src/lib/ui/Link.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	export let href: string;
+
+	$: external = !(href.startsWith('/') || href.startsWith('.'));
+	$: rel = external ? 'noreferrer' : undefined;
+	$: target = external ? '_blank' : undefined;
 </script>
 
-<a rel="noreferrer" {...$$restProps} {href}><slot /></a>
+<a {...$$restProps} {href} {target} {rel}><slot /></a>
