08facb02f9a845710008c701d7f470d00e8c0ee6
Ryan Atkinson <mail@ryanatkn.com>
2022-07-19 21:24:11 -0500

    add nofollow

--- src/lib/ui/Link.svelte
+++ src/lib/ui/Link.svelte
@@ -2,8 +2,9 @@
 	export let href: string;
 
 	$: external = !(href.startsWith('/') || href.startsWith('.'));
-	$: rel = external ? 'external noreferrer' : undefined;
+	$: rel = external ? 'external noreferrer nofollow' : undefined;
 	$: target = external ? '_blank' : undefined;
 </script>
 
+<!-- TODO maybe user `sveltekit:prefetch` for internal links -->
 <a {...$$restProps} {href} {rel} {target}><slot /></a>
